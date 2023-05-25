4f85c81824fd1e123f5293df74fc1cb44576fd49
peter <petersp2000@gmail.com>
2023-05-25 18:00:02 +0100

    subscriber and publisher'

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,18 @@
+import { createClient, print } from 'redis';
+
+
+const client = createClient();
+
+client.on('error', err => console.log(`Redis client not connected to the server: ${err}`));
+client.on('connect', () => console.log("Redis client connected to the server"));
+
+
+client.subscribe("holberton school channel")
+client.on("message", (channel, message) => {
+    if (channel === "holberton school channel") {
+        if (message == "KILL_SERVER") {
+            client.unsubscribe("holberton school channel");
+        }
+        console.log(message);
+    }
+})
